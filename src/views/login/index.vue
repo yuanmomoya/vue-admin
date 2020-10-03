1c8f7a9e0ddf3a7014e1e9319ddd58c82c6d7965
yuanmomo <943366515@qq.com>
2020-10-03 09:32:41 +0800

    初始化修改提交

---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="login">这里是登陆页面</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
